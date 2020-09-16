4b835530e0f091b908c37f2c1811c1354691f6a7
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-16 15:33:49 +0200

    Added the operations achievement

---
+++ PythonAchievements/operations.py
@@ -0,0 +1,35 @@
+Name = "Mikey"
+thirty = 30
+fifty = 50
+seventy = 70
+
+print(fifty + fifty)
+
+print(fifty - thirty)
+
+print(seventy / thirty)
+
+number1 = 5 + 5
+print(number1)
+number1 += 5
+print(number1)
+
+number2 = 10 + 10
+print(number2)
+number2 /= 10
+print(number2)
+
+print(Name)
+
+("What about a leap year?")
+
+year_input = int(input("Enter a year:  "))
+ 
+if year_input % 4 == 0:
+ 
+   print("Remainder is 0 so ", year_input, "is a leap year!")
+ 
+else:
+ 
+   print(year_input, "Not a leap year!")
+
