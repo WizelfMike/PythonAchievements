0c823499b842cd5c164f2680100bd5d304ff61f2
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-16 14:30:14 +0200

    Made the player stats achievement

---
+++ PythonAchievements/Player_Stats.py
@@ -0,0 +1,27 @@
+print("What is your name?")
+name = input()
+
+print("What is your age?")
+age = input()
+
+print("What is your gender?")
+gender = input()
+
+print("From a scale of 1-10, how strong are you?")
+strength = input()
+
+print("From a scale of 1-10, how clever are you?")
+intelect = input()
+
+print("How has you character's life been so far?")
+life_quality = input()
+
+print(" ")
+
+print("You player stats are:")
+print("Name= " + name)
+print("Age= " + age)
+print("Gender= " + gender)
+print("Strenght= " + strength)
+print("Intelligence= " + intelect)
+print("Status of life= " + life_quality)
