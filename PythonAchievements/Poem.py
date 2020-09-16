028fc7f9bff69f1926a410609dd9b1a123c6120b
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-16 13:58:13 +0200

    added the Poem to the achievements

---
+++ PythonAchievements/Poem.py
@@ -0,0 +1,7 @@
+vers1 = "My name is Mikey Clarke and i am 16."
+vers2 = "I enjoy internet content, especially Killer Bean."
+vers3 = "In terms of games, Team Fortress 2 is a must."
+vers4 = "In Valve we believe, and in Gaben we trust."
+vers5 = "Be nice to me and i'll be nice back."
+vers6 = "However mistreat me and i will snap."
+print(vers1, vers2, vers3, vers4, vers5, vers6)
