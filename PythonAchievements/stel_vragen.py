769bd647bbaa2b3f660da936561d5192fb8b2b99
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-23 15:27:10 +0200

    Added the output achievement

---
+++ PythonAchievements/stel_vragen.py
@@ -0,0 +1,13 @@
+Name = input("What's your name? ")
+
+print(Name + " Huh? Cool name!")
+
+
+Age = input("How old are you? ")
+
+print("You're age is " + Age + ". Bit old don't you  think?")
+
+
+Activity = input("What do you like to do? ")
+
+print(Activity + ", cool, sounds like fun!")
