6fdb070b4cd35394ee3cd2150ce75d7c56388f93
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-30 15:00:16 +0200

    Made the family Ties

---
+++ PythonAchievements/Family_Ties.py
@@ -0,0 +1,8 @@
+name = "Mikey"
+age = "16"
+area = "Hillegom"
+distance = "23.1 KM"
+game = "Celeste"
+
+
+print("My name is " + name + ", i am " + age + " years old and i live in " + area + ". I live " + distance + " away from school, and my favourite game is " + game + ".") 
