d61ff36edcbe59fa142d1a79fae6a19df28a9c3b
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-10-07 15:02:11 +0200

    Made the Slice script

---
+++ PythonAchievements/Slice_Wise.py
@@ -0,0 +1,44 @@
+
+
+Tekst = "De Python lessen worden gegeven door Erik, Erwin en ook door Hidde"
+
+D1 = Tekst[0:37]
+D2 = Tekst[48:61]
+print(D1, D2)
+
+
+#-----------------------------
+
+tekst = "SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ..."
+
+F1 = tekst[0:15]
+F2 = tekst[71:91]
+
+
+print(F1, F2)
+
+
+#----------------------------
+
+
+tekst2 = "Wat is hier het laatste woord?"
+
+G = tekst2[0:23]
+
+print(G)
+
+
+
+#----------------------------
+
+
+tekst3 = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+
+H1 = tekst3[0:4]
+H2 = tekst3[8:28]
+H3 = tekst3[32:63]
+
+print(H1, H2, H3)
+
+
+#----------------------------
