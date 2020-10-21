084ab42a3ac4057464be9a9057ffc344bec97b5e
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-10-21 12:51:34 +0200

    Variableeees, fuck yea!

---
+++ Ma/Bewijzenmap/X 1/Y 1 (Hello You)/PythonAchievements/Variables.py
@@ -0,0 +1,20 @@
+Char = ("H")
+
+x = float(5.3000)
+
+Bool1 = (bool(0))
+Bool2 = (Bool1 < 1)
+
+int1 = (3)
+int2 = (1)
+int3 = (int1 - int2)
+
+Str1 = ("Hey, my name is ")
+Str2 = ("Mikey Clarke")
+Str3 = (Str1 + Str2)
+
+print(Char)
+print(x)
+print(Bool2)
+print(int3)
+print(Str3)
