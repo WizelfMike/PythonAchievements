b0fe2a4c0d45a6517b3ce3fa27ec3cc1bc42c6f8
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-10-28 14:45:20 +0100

    Made some more achievements

---
+++ Ma/Bewijzenmap/X 1/Y 1 (Hello You)/PythonAchievements/Daily_Choices.py
@@ -0,0 +1,66 @@
+print("Hey, how was your day?")
+choice = input()
+if choice == 'bad' :
+    print("Sorry to hear that, i hope it gets better!")
+elif choice == 'good' :
+    print("That's good to hear!")
+else :
+    print("Please type an actual answer")
+
+print() 
+input("Press Enter")
+
+print("What would you say is your favourite? red, green or blue?")
+choice2 = input()
+if choice2 == 'blue' :
+    print("That's a lovely colour, Skies and Seas.")
+elif choice2 == 'green' :
+    print("Oooo, i like green. It's my favourite too!")
+elif choice2 == 'red' :
+    print("Blood")
+else :
+    print("Please pick a colour")
+
+print()
+input("Press Enter")
+
+print("Are you a man or woman?")
+choice3 = input()
+if choice3 == 'woman' : 
+    print("Hello m'lady")
+elif choice3 == 'man' :
+    print("Hey dude")
+else :
+    print("Please pick a gender")
+print()
+
+
+
+print()
+input("Please press enter")
+
+
+print("What do you choose? A. B or C?")
+choice4 = input()
+if choice4 == 'A' :
+    print("A is for alpha")
+elif choice4 == 'B' :
+    print ("B is for Beta")
+elif choice4 == 'C' :
+    print("C is for... Something i can't say")
+else :
+    print("Pick a letter please")
+
+print()
+
+print()
+input()
+
+print("work or home?")
+choice5 = input()
+if choice5 == 'work' :
+    print("Weird, most would pick home!")
+elif choice5 == 'home' :
+    print("Yea same honestly")
+else :
+    print ("please pick of the given locations")
---
+++ Ma/Bewijzenmap/X 1/Y 1 (Hello You)/PythonAchievements/Shuffle_and_Return.py
@@ -0,0 +1,9 @@
+import random
+
+def List(names):
+    randomised = ''.join(random.sample(names, len(names)))
+    print(randomised)
+
+
+names = input()
+List(names)
